fdb54ba9d1c6d25c83eb1a8b8d6a92dfa14a4776
henriquegouveia1 <henriquegs002@gmail.com>
2022-12-22 16:26:51 -0300

    ajeitando o ultimo commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
+import BoardGame.Board;
 import BoardGame.Position;
 
 public class Main {
     public static void main(String[] args) {
 
-        Position pos = new Position(3,5);
+        Board board = new Board(8,8);
 
-        System.out.println(pos);
     }
 }
