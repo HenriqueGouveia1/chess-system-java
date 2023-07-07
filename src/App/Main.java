6cea66fe1b2f1ee688b326e4d407fb52b8fc0b3c
henriquegouveia1 <henriquegs002@gmail.com>
2023-07-07 15:57:02 -0300

    Moving pieces implemented

--- src/App/Main.java
+++ src/App/Main.java
@@ -1,13 +1,30 @@
 package App;
+import java.util.Scanner;
+
 import BoardGame.Board;
 import BoardGame.Position;
 import Chess.ChessMatch;
+import Chess.ChessPiece;
+import Chess.ChessPosition;
 
 public class Main {
     public static void main(String[] args) {
 
          ChessMatch cm = new ChessMatch();
-         UI.printBoard(cm.getPieces());
+         Scanner sc = new Scanner(System.in);         
+
+         while (true) {
+            UI.printBoard(cm.getPieces());
+            
+            System.out.println();
+            System.out.println("Source:");
+            ChessPosition source = UI.readChessPosition(sc);
+            System.out.println("target:");
+            ChessPosition target = UI.readChessPosition(sc);
+
+            ChessPiece capturedPiece = cm.performChessMove(source, target);
+            
+         }
          
          
     }
